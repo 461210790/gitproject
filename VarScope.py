aa9d42548abf2decde1ec5d93c221db48438bfb0
liv <461210790@qq.com>
2021-11-11 10:42:26 +0800

    second

--- VarScope.py
+++ VarScope.py
@@ -2,6 +2,9 @@ import moduleVar
 number=200   #第一个number作用域类型是什么？
 print(number)
 print(moduleVar.number) #第二个number作用域类型是什么？
+print(moduleVar.number) #第二个number作用域类型是什么？
+print(moduleVar.number) #第二个number作用域类型是什么？
+print(moduleVar.number) #第二个number作用域类型是什么？
 def function1():
     global number
     print(number)
